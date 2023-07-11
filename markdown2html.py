b1df483efd4e65686ba193cf6bf073336e4da9a6
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-07-11 19:32:04 +0000

    0. Start a script

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script that converts Markdown to HTML.
+"""
+
+import sys
+import os
+import re
+
+def convert_markdown_to_html(input_file, output_file):
+    """
+    Converts a Markdown file to HTML and writes the output to a file.
+    """
+    # Check that the Markdown file exists and is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
